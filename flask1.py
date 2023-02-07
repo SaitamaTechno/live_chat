14b20b9d3a6640dea93dc326db38d72bd46e5b47
mypi <halukenestelli143@gmail.com>
2023-02-07 15:00:44 +0300

    deleted trash lines

--- flask1.py
+++ flask1.py
@@ -5,17 +5,12 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-messages = []
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @socketio.on("message sent")
 def handle_message(data):
-    messages.append(f"{data['username']}: {data['message']}")
-    if len(messages) > 30:
-        messages.pop(0)
     emit("new message", {"username": data["username"], "message": data["message"]}, broadcast=True)
 
 if __name__ == "__main__":
